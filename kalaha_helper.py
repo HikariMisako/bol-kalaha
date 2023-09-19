10419d0d19cb26f7e72075611ac760e950717058
Jeroen v Hoek <jvanhoek@gmail.com>
2023-09-19 08:57:32 +0200

    change score print to sideways vieww

--- kalaha_helper.py
+++ kalaha_helper.py
@@ -13,31 +13,50 @@ def create_player_pits(
     return pit_list
 
 
-def create_score_overview(current_player: bool, all_pits: list[BallPit], game_is_done: [bool, str]) -> dict:
+def create_score_overview(
+    current_player: bool, all_pits: list[BallPit], game_is_done: [bool, str]
+) -> dict:
     """
     :return: Returns a dictionary containing a human-readable game state with:
     - Current number of balls for each pit
-    - A line for each player in the kalaha "board" format
+    - A line for each player in the kalaha "board" format, sideways!
+        - Displaying for each pit, the index and the number of balls
     - Whose turn it is
+
+    Display is sideways, with player 1 on the left and player 2 on the right.
+    Example with 2 pits and 2 balls starting:
+    "00": "I:      P1     P2     :I",
+    "01": "LARGE>         0      :05",
+    "02": "00:     2      2      :04",
+    "03": "01:     2      2      :03",
+    "04": "02:     0      <LARGE",
     """
-    return_dict = {
-        # the pit index is useful for play, so we add both the index and the short string to the output
-        "pits": "  ".join(
-            [f"{i}:{all_pits[i].short_str()}" for i in range(len(all_pits))]
-        )
-    }
+    # this format is far from ideal, however once a proper display format is decided this can be reworked
+    lines = ["I:      P1     P2     :I"]
 
-    player_a_pits = [pit for pit in all_pits if pit.match_player(True)]
-    player_b_pits = [pit for pit in all_pits if pit.match_player(False)]
+    # for each pit in the list, we want to display the index and the current number of balls.
+    for i in range(int(len(all_pits) / 2)):
+        mirror_i = len(all_pits) - i - 2
+        p1_pit = all_pits[i]
+        p2_pit = all_pits[mirror_i]
+        # first 2 lines are an exception
+        if i == 0:
+            lines.append(
+                f"LARGE>         {all_pits[mirror_i + 1].get_ball_count()}      :{mirror_i + 1:02}"
+            )
+            lines.append(
+                f"{i:02}:     {p1_pit.get_ball_count()}      {p2_pit.get_ball_count()}      :{mirror_i:02}"
+            )
+        elif p1_pit.is_large():
+            lines.append(f"{i:02}:     {p1_pit.get_ball_count()}      <LARGE")
+        else:
+            lines.append(
+                f"{i:02}:     {p1_pit.get_ball_count()}      {p2_pit.get_ball_count()}      :{mirror_i:02}"
+            )
 
-    player_b_line = "  ".join(
-        [str(pit.get_ball_count()) for pit in player_b_pits][::-1]
-    )
-    player_a_line = "   " + "  ".join(
-        [str(pit.get_ball_count()) for pit in player_a_pits]
-    )
-    return_dict["player_2"] = player_b_line
-    return_dict["player_1"] = player_a_line
+    return_dict = {}
+    for i in range(len(lines)):
+        return_dict[f"{i:02}"] = lines[i]
 
     if current_player:
         return_dict["turn"] = "Current player: FIRST PLAYER"
